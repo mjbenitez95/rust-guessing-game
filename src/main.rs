20d0947792408f3deafe27767b4225546753d43d
Matthew Benitez <matthewjbenitez95@gmail.com>
2021-01-28 15:28:37 -0800

    cargo fmt

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,22 @@
-use std::cmp::Ordering;
 use rand::Rng;
+use std::cmp::Ordering;
 
-fn make_guess(min: u64, max: u64) -> u64{
+fn make_guess(min: u64, max: u64) -> u64 {
     return (min + max) / 2;
 }
 
 fn main() {
-
     let mut min = 1;
     let mut max = 100;
     let count = 0;
-    let secret_number = rand::thread_rng().gen_range(min, max+1);
+    let secret_number = rand::thread_rng().gen_range(min, max + 1);
     println!("Secret number is: {}.", secret_number);
-    
+
     loop {
         let count = count + 1;
         let guess = make_guess(min, max);
 
         println!("Binary search guess #{} is: {}.", count, guess);
-        
 
         match guess.cmp(&secret_number) {
             Ordering::Less => {
